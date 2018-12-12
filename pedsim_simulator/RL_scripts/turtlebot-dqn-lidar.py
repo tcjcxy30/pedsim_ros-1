841f429bc41e04d144b55b7e815709305e14e153
Jongseo Yoon <js0823@gmail.com>
2018-12-12 00:25:03 -0500

    Starting dqn

--- pedsim_simulator/RL_scripts/turtlebot-dqn-lidar.py
+++ pedsim_simulator/RL_scripts/turtlebot-dqn-lidar.py
@@ -1,1 +1,13 @@
 # Python file for launching DQN using lidar to learn.
+
+import numpy as np
+import matplotlib as plt
+from keras.models import Sequential, load_model
+from keras import optimizers
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+from keras.regularizers import l2
+import time
+import random
+import os
